3e7dd5991571eb9156fd803ade24ccee5a2a7b94
Viram Keshwala <viram.k@sgit.in>
2018-03-27 18:54:19 +0530

    app.config

--- src/Controllers/main.js
+++ src/Controllers/main.js
@@ -1,1 +1,17 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp",["ui.router"]);
+app.config(function($stateProvider, $urlRouterProvider){
+
+    $stateProvider.state({
+        name: 'Page2',
+        url: '/Page2',
+        templateUrl: '/Views/Page2.html'
+    });
+
+    $stateProvider.state({
+        name: 'Index',
+        url: '/Index',
+        templateUrl: '/Views/Page1.html'
+    });
+
+    $urlRouterProvider.otherwise('/Index');
+});
